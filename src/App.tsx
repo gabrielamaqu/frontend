bfa43f801e9fa3a1ada44974bc2c773ea9c04980
Kaique Medeiro <kaique.medeiro14@hotmail.com>
2023-11-24 14:45:37 -0300

    adicionado Layout, feito css Layout, e executado exer 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import { Route, Routes } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Home from './pages/home';
+import Cart from './pages/cart';
+import Layout from './components/layout';
 
 function App() {
+  const [listProducts, setListProducts] = useState<[]>([]);
+  const [listProductsCart, setListProductsCart] = useState<[]>([]);
   return (
     <Routes>
-      {/* {Se encontra no src/pages} */}
-      <Route path="/" element={ <Home /> } />
-      <Route path="/cart" element={ <Cart /> } />
+      <Route path="/" element={ <Layout /> }>
+        <Route
+          index
+          element={ <Home
+            listProducts={ listProducts }
+            setListProductsCart={ setListProductsCart } 
+          /> }
+        />
+        <Route path="/cart" element={ <Cart listProductsCart={ listProductsCart } /> } />
+      </Route>
     </Routes>
   );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,21 @@
-import { useState } from 'react';
+import { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
 import './home.css';
 
-function Home() {
-  // lista de produtos, ainda não temos o type da lista, após ter, colocar a tipagem no useState<Type[]>
-  const [listProducts, setListProducts] = useState<[]>([]);
+type HomeProps = {
+  listProducts: [];
+  setListProductsCart: React.Dispatch<React.SetStateAction<[]>>;
+};
+
+function Home({ listProducts, setListProductsCart }: HomeProps) {
+  // o tipo da lista de produtos no carrinho ainda não temos, após ter o ProductType, colocar a tipagem no useState<Type[]>
+
+  // const handleAddToCart = () => {
+  //   setListProductsCart([1]);
+  // }
 
   return (
     <div className="home-page">
-      <input className="input-home" type="text" />
       {listProducts.length === 0 ? (
         <h3 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
@@ -15,8 +23,10 @@ function Home() {
       ) : (
         // Renderizar a lista de produtos quando não estiver vazia
         // Coloque aqui o código para renderizar a lista de produtos
+        // Apague este h3 abaixo após pegar a lista
         <h3>Lista com produtos</h3>
       )}
+      {/* <button onClick={ handleAddToCart }>ADD TO CAR</button> */}
     </div>
   );
 }
